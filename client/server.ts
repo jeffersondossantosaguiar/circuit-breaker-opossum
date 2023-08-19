97fe8dafef23d4c58e60e774f8ef734f1d0e4299
Jefferson Aguiar <jefferson.aguiar@facens.br>
2023-08-19 19:19:26 -0300

    refact: optimized code

--- client/server.ts
+++ client/server.ts
@@ -5,25 +5,34 @@ import { createClient } from "redis"
 
 const app = express()
 const redisClient = createClient()
+const redisTTL = 1 * 60 //one minute
 const cacheKey = "api-response"
 
 redisClient.on("error", (err) => console.log("Redis Client Error", err))
 redisClient.connect()
 
 const circuitBreakerOptions = {
-  timeout: 3000, // Tempo limite para uma solicitação antes de acionar o fallback
+  timeout: 3000, // Timeout to call fallback Tempo limite para uma solicitação antes de acionar o fallback
   errorThresholdPercentage: 50, // Porcentagem de erros antes de acionar o fallback
   resetTimeout: 10000 // Tempo de espera antes de tentar reabrir o circuito
 }
 
-function asyncFunctionThatCouldFail() {
-  return axios.get("http://localhost:3001")
+async function asyncHttpCall() {
+  const cacheResponse = await redisClient.get(cacheKey)
+
+  if (cacheResponse) return JSON.parse(cacheResponse)
+
+  const response = await axios.get("http://localhost:3001")
+
+  await redisClient.set(cacheKey, JSON.stringify(response.data), {
+    EX: redisTTL, //expire time, in seconds.
+    NX: true //only set the key if it does not already exist
+  })
+
+  return response.data
 }
 
-const circuitBreaker = new CircuitBreaker(
-  asyncFunctionThatCouldFail,
-  circuitBreakerOptions
-)
+const circuitBreaker = new CircuitBreaker(asyncHttpCall, circuitBreakerOptions)
 
 circuitBreaker
   .on("close", () => console.log("CLOSE"))
@@ -31,31 +40,17 @@ circuitBreaker
   .on("halfOpen", () => console.log("HALF"))
   .on("success", () => console.log("SUCCESS"))
   .on("fallback", () => console.log("FALLBACK"))
+  .fallback(() => {
+    return { id: 1, title: "iphone", price: "2.000" } //default object to return on fallback, here we can adopt any strategy
+  })
 
-// Endpoint para consulta na API externa
+// External endpoint to call
 app.get("/", async (req, res) => {
-  circuitBreaker
-    .fallback(async () => {
-      const data = await redisClient.get(cacheKey)
+  const response = await circuitBreaker.fire()
 
-      if (!data) throw new Error("Erro Interno")
-
-      return JSON.parse(data)
-    })
-    .fire()
-    .then((response) => {
-      let result = response
-      if (response.data) {
-        result = response.data
-        redisClient.set(cacheKey, JSON.stringify(response.data))
-      }
-      res.send(result)
-    })
-    .catch((error) => {
-      res.status(500).send(error.message)
-    })
+  res.send(response)
 })
 
 app.listen(8080, () => {
-  console.log("Servidor rodando na porta 8080!")
+  console.log("Server running at door 8080!")
 })
